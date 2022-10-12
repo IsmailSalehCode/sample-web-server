dc1cf4686dee755cc110cfb535c19cb7efdd26fb
IsmailSalehCode <centriu78@gmail.com>
2022-10-13 01:51:17 +0300

    s

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-const corsOptions_8080 = {
-  origin: "http://localhost:8080",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
 };
-
+app.use(cors(corsOptions));
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.listen(port);
 
 console.log("Server started at http://localhost:" + port);
-app.get("/", cors(corsOptions_8080), function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
